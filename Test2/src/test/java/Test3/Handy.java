efe095a52a46b5b5e0edab7497613284a10216ff
Shyhdurdy <shyhdurdy.o@gmail.com>
2019-08-21 08:32:34 -0400

    firstttdd

---
+++ Test2/src/test/java/Test3/Handy.java
@@ -0,0 +1,14 @@
+package Test3;
+
+public abstract class Handy {
+	public abstract void many();
+	
+	public void ne() {
+		System.out.println("anythins");
+		
+	}
+private void mn() {
+	System.out.println();
+	
+}
+}
